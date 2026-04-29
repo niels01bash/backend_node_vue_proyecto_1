a1069b27f869db6811dfbca71be0c35619a7bef6
Niels <your_email@example.com>
2026-04-29 02:46:19 -0400

    backend CRUD api usuario

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -50,10 +50,48 @@ export default {
         }
 
     },
-    funModificar(req, res){
+    async funModificar(req, res){
+        //const id = req.params.id;
+        const { id } = req.params;
+        const { name, email, password } = req.body ;
+        try {
+            const user = await models.User.findByPk(id);
 
+            if(!user){
+                return res.status(404).json({error: 'Usuario No encontrado'});
+            }
+            // modificar
+            if (name) {
+                user.name = name;
+            }
+            if (email) user.email = email;
+            if (password) {
+                user.password = await bcrypt.hash(password, 12);
+            };
+
+            // guardar
+            await user.save();
+
+            return res.status(200).json({mensaje: "Usuario actualizado", usuario: {id: user.id, name: user.name, email: user.email}});
+            
+        } catch (error) {
+            return res.status(500).json({error: "Error al modificar el usuario"});
+        }
     },
-    funEliminar(req, res){
+    async funEliminar(req, res){
+        const{ id }= req.params;
+        try {
+            const user = await models.User.findByPk(id);
+            if(!user){
+                return res.status(404).json({error: 'Usuario No encontrado'});
+        } 
+            //await user.detroy 
+            return res.status(200).json({mensaje: "Usuario eliminado"});
+
+
+        }catch(error){
+            return res.status(500).json({error:"Error al eliminar el usuario"});
+        }
 
     }
 }
